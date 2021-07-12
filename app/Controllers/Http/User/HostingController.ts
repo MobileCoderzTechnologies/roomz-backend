e35b22790d9adad3b913a54bd8e5b02777ecf1ec
pukhraj saini <pukhraj@mobilecoderz.com>
2021-07-12 13:26:33 +0530

    id in bed-types and property-types api

--- app/Controllers/Http/User/HostingController.ts
+++ app/Controllers/Http/User/HostingController.ts
@@ -16,24 +16,28 @@ export default class HostingController {
     * @apiVersion 1.0.0
     * @apiName BedTypes
     * @apiGroup Hosting
-    *  *
+    *
     * @apiSuccessExample {json} Success-Response:
     *     HTTP/1.1 200 OK
     *     {
     *       "data": [
     *      {
+    *           "id": 1,
     *          "uid": "be4805d2-5782-4a8c-af27-9fc70da0dd65",
     *          "bed_type": "double"
     *      },
     *      {
+    *           "id": 2,
     *          "uid": "c1cfaaef-db8e-4928-8d86-1f25999fb5d6",
     *          "bed_type": "queen"
     *      },
     *      {
+    *          "id": 3,
     *          "uid": "c82267e0-5a11-49b2-9592-e48c5f8f4fbc",
     *          "bed_type": "single"
     *      },
     *      {
+    *          "id": 4,
     *          "uid": "878bd05f-194f-4e84-b8f0-e2953f9f8517",
     *          "bed_type": "sofa bed"
     *      }
@@ -50,7 +54,7 @@ export default class HostingController {
   async getBedTypes({ response }: HttpContextContract) {
     try {
       const bedList = await BedType.query()
-        .select('uid', 'bed_type')
+        .select('id', 'uid', 'bed_type')
         .finally();
 
       return response.status(Response.HTTP_OK).json({
@@ -75,32 +79,38 @@ export default class HostingController {
     * @apiVersion 1.0.0
     * @apiName PropertyTypes
     * @apiGroup Hosting
-    *  *
+    *
     * @apiSuccessExample {json} Success-Response:
     *     HTTP/1.1 200 OK
     *     {
     *    "data": [
     *      {
+    *        "id": 1,
     *        "uid": "af1ebb9a-80a1-4573-8ba5-98d6a81f3210",
     *        "property_type": "Villa"
     *      },
     *      {
+    *         "id":2
     *        "uid": "43d21645-e458-4eed-8db5-d08cde2bdfcd",
     *        "property_type": "Apartment"
     *      },
     *      {
+    *       "id":3,
     *        "uid": "c12c0a8f-c07f-46fc-804f-ab198b4e7304",
     *        "property_type": "Farm House"
     *      },
     *      {
+    *         "id":4,
     *        "uid": "bb178ee0-d880-4fda-881d-802c0aaa0f22",
     *        "property_type": "Istiraha"
     *      },
     *      {
+    *         "id": 5,
     *        "uid": "55133720-6407-4e1a-9fa7-c278e1da85c1",
     *        "property_type": "Camp"
     *      },
     *      {
+    *         "id":6
     *        "uid": "2a2391b9-a595-4853-a538-2f6cc5c46263",
     *        "property_type": "Heritage House"
     *      }
@@ -114,21 +124,21 @@ export default class HostingController {
     *      }
     *
     */
-async getPropertyTypes({ response }: HttpContextContract) {
-  try {
-    const propertyList = await PropertyType.query()
-      .select('uid', 'property_type')
-      .finally();
-    return response.status(Response.HTTP_OK).json({
-      data: propertyList
-    })
-  } catch (error) {
-    console.log(error)
-    return response.status(Response.HTTP_INTERNAL_SERVER_ERROR).json({
-      message: t('Something went wrong')
-    });
+  async getPropertyTypes({ response }: HttpContextContract) {
+    try {
+      const propertyList = await PropertyType.query()
+        .select('id', 'uid', 'property_type')
+        .finally();
+      return response.status(Response.HTTP_OK).json({
+        data: propertyList
+      })
+    } catch (error) {
+      console.log(error)
+      return response.status(Response.HTTP_INTERNAL_SERVER_ERROR).json({
+        message: t('Something went wrong')
+      });
+    }
   }
-}
 
 
 }
