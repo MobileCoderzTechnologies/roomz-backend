d5fe02ca12bf2f58d4dda2f627a4847b985d7db5
pukhraj saini <pukhraj@mobilecoderz.com>
2021-08-12 09:32:40 +0530

    image sizes fixex

--- app/Constants/ImageSizesConstant.ts
+++ app/Constants/ImageSizesConstant.ts
@@ -1,37 +1,37 @@
 export const IMAGE_SIZES = [
   {
     imagePath: "334x192",
-    height: 334,
-    width: 192
+    height: 192,
+    width: 334
   },
   {
     imagePath: "160x150",
-    height: 160,
-    width: 150
+    height: 150,
+    width: 160
   },
   {
     imagePath: "375x280",
-    height: 375,
-    width: 280
+    height: 280,
+    width: 375
   },
   {
     imagePath: "125x100",
-    height: 125,
-    width: 100
+    height: 100,
+    width: 125
   },
   {
     imagePath: "328x202",
-    height: 328,
-    width: 202
+    height: 202,
+    width: 328
   },
   {
     imagePath: "235x158",
-    height: 235,
-    width: 158
+    height: 158,
+    width: 235
   },
   {
     imagePath: "576x250",
-    height: 576,
-    width: 250
+    height: 250,
+    width: 576
   }
 ];
